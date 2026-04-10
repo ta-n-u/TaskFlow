3b676fbf25b8b5fa7fedd00f24c36106e5175ad9
Tanu Yadav <tanu0yadav@gmail.com>
2026-04-10 11:29:46 +0545

    Add AppDbContext, EF Core setup, and initial migration

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,6 +1,22 @@
-﻿namespace TaskFlow.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskFlow.Models
 {
     public class TaskItem
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        [Required]
+        public string Priority { get; set; } = "Medium";
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
